06ef6bea830fdbfb060a29455b4fb1da5585bd54
frankbouwens <5103a8425916473cd19e8fd6d4b7118c92aba882>
2020-05-18 23:24:19 +0200

    Add some native code

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Frank Bouwens on 18/05/2020.
+//
+
+#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_nl_frankkie_androidgithubactions_Util_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
--- app/src/main/java/nl/frankkie/androidgithubactions/Util.kt
+++ app/src/main/java/nl/frankkie/androidgithubactions/Util.kt
@@ -2,10 +2,19 @@ package nl.frankkie.androidgithubactions
 
 class Util {
 
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
     companion object {
         fun validateOnlyNonCapital(text: String): Boolean {
             return (text == text.toLowerCase())
         }
-    }
 
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
 }
